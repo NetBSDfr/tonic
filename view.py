7310e7fe1050233b45b5a0dde8b61aef05cd0188
Guillaume 'Llew' Delpierre <gde@llew.me>
2013-04-19 17:23:00 +0200

    change type of splitting windows

--- view.py
+++ view.py
@@ -7,56 +7,11 @@ def timenow():
     """ """
     return time.time()
 
-class VSplitterPanel(wx.Panel):
-    """ Constructs a Vertical splitter window with left and right panels"""
-    def __init__(self, parent, color):
-        """ """
-        wx.Panel.__init__(self, parent)
-        self.SetBackgroundColour(color)
-        splitter = wx.SplitterWindow(self, style = wx.SP_3D| wx.SP_LIVE_UPDATE)
-        leftPanel = wx.Panel(splitter)
-        rightPanel = wx.Panel(splitter)
-        leftPanel.SetBackgroundColour('SEA GREEN')
-        rightPanel.SetBackgroundColour('STEEL BLUE')
-
-        splitter.SplitVertically(leftPanel, rightPanel) 
-        PanelSizer=wx.BoxSizer(wx.VERTICAL)
-        PanelSizer.Add(splitter, 1, wx.EXPAND | wx.ALL)
-        self.SetSizer(PanelSizer)
-
-class HSplitterPanel(wx.Panel):
-    """ Constructs a Horizontal splitter window with top and bottom panels"""
-    def __init__(self, parent, color):
-        """ """
-        wx.Panel.__init__(self, parent)
-        self.SetBackgroundColour(color)
-        splitter = wx.SplitterWindow(self, style = wx.SP_3D| wx.SP_LIVE_UPDATE)
-        TopPanel = wx.Panel(splitter)
-        BottomPanel = wx.Panel(splitter)
-        TopPanel.SetBackgroundColour('YELLOW GREEN')
-        BottomPanel.SetBackgroundColour('SLATE BLUE')
-
-        splitter.SplitHorizontally(TopPanel, BottomPanel) 
-        PanelSizer=wx.BoxSizer(wx.VERTICAL)
-        PanelSizer.Add(splitter, 1, wx.EXPAND | wx.ALL)
-        self.SetSizer(PanelSizer)
-
 class View(wx.Frame):
     """ HMI for Tonic """
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title,
                               pos=(-1, -1), size=(600,400))
-
-        # define mainsplitter as child of frame and add H/V splitterPanel
-        # as children
-        mainsplitter = wx.SplitterWindow(self, 
-                                         style = wx.SP_3D| wx.SP_LIVE_UPDATE)
-        splitterpanel1 = HSplitterPanel(mainsplitter,'LIGHT BLUE') 
-        splitterpanel2 = VSplitterPanel(mainsplitter,'LIGHT BLUE') 
-        mainsplitter.SplitHorizontally(splitterpanel2, splitterpanel1)
-        MainSizer = wx.BoxSizer(wx.VERTICAL)
-        MainSizer.Add(mainsplitter, 1, wx.EXPAND | wx.ALL)
-        self.SetSizer(MainSizer)
 
         # Creating the statusbar in the bottom of the window
         self.CreateStatusBar()
@@ -120,7 +75,25 @@ class View(wx.Frame):
                                          "Unmark All", "")
         # show the toolbar to the frame 
         toolbar.Realize()
-       
+
+
+        # Split the windows
+        splitter = wx.SplitterWindow(self, style = wx.SP_BORDER)
+        leftPanel = wx.Panel(splitter)
+        rightPanel = wx.Panel(splitter)
+
+        localTree = wx.TreeCtrl(leftPanel)
+        leftSizer = wx.BoxSizer(wx.VERTICAL)
+        leftSizer.Add(localTree, 1, wx.EXPAND | wx.ALL)
+        leftPanel.SetSizer(leftSizer)
+
+        flickrTree  = wx.TreeCtrl(rightPanel)
+        rightSizer = wx.BoxSizer(wx.VERTICAL)
+        rightSizer.Add(flickrTree, 1, wx.EXPAND | wx.ALL)
+        rightPanel.SetSizer(rightSizer)
+
+        splitter.SplitVertically(leftPanel, rightPanel)      
+ 
         # Tadam 
         self.Refresh()
         self.Show(True)
