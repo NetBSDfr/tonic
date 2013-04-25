9d1fab1abf7d0d40f93f920f77cf55a7eb28435e
Guillaume 'Llew' Delpierre <gde@llew.me>
2013-04-25 22:01:14 +0200

    Add text box into description box

--- tonic/widgets/view.py
+++ tonic/widgets/view.py
@@ -35,35 +35,47 @@ class TonicView(wx.Frame):
     """HMI for Tonic"""
     def __init__(self, parent, title):
         """Constructor."""
-        wx.Frame.__init__(self, parent, title=title,
+        wx.Frame.__init__(self, parent, title=title,\
                           pos=(-1, -1), size=(800,600))
         # Creating the categories list
-        self.list_category = widgets.TonicCatListCtrl(self,
-                                                      style=wx.LC_REPORT|
-                                                      wx.LC_NO_HEADER|
-                                                      wx.LC_LIST|
+        self.list_category = widgets.TonicCatListCtrl(self,\
+                                                      style=wx.LC_REPORT|\
+                                                      wx.LC_NO_HEADER|\
+                                                      wx.LC_LIST|\
                                                       wx.SUNKEN_BORDER)
 
         # Creating the searchbox
         self.search_box = wx.SearchCtrl(self, -1, "")
 
         # Creating the packages list
-        self.list_pkg = widgets.TonicPkgListCtrl(self,
-                                                 style=wx.LC_REPORT|
+        self.list_pkg = widgets.TonicPkgListCtrl(self,\
+                                                 style=wx.LC_REPORT|\
                                                  wx.SUNKEN_BORDER)
 
         # Creating suppl. informations about package panel
         self.description_tab = wx.Notebook(self, -1, style=wx.NB_BOTTOM)
-        self.description_tab_desc = wx.Panel(self.description_tab, -1,
-                                             style=wx.SUNKEN_BORDER|
+        self.description_tab_desc = wx.Panel(self.description_tab, -1,\
+                                             style=wx.SUNKEN_BORDER|\
                                              wx.TAB_TRAVERSAL)
-        self.description_tab_dep = wx.Panel(self.description_tab, -1,
-                                            style=wx.SUNKEN_BORDER|
+        self.description_tab_dep = wx.Panel(self.description_tab, -1,\
+                                            style=wx.SUNKEN_BORDER|\
                                             wx.TAB_TRAVERSAL)
-        self.description_tab_chg = wx.Panel(self.description_tab, -1,
-                                            style=wx.SUNKEN_BORDER|
+        self.description_tab_chg = wx.Panel(self.description_tab, -1,\
+                                            style=wx.SUNKEN_BORDER|\
                                             wx.TAB_TRAVERSAL)
 
+        self.text_tab_desc - wx.TextCtrl(self.description_tab_desc,\
+                                        -1, "",\
+                                        style=wx.TE_MULTILINE|\
+                                              wx.TE_READONLY)
+        self.text_tab_dep - wx.TextCtrl(self.description_tab_dep,\
+                                        -1, "",\
+                                        style=wx.TE_MULTILINE|\
+                                              wx.TE_READONLY)
+        self.text_tab_chg - wx.TextCtrl(self.description_tab_chg,\
+                                        -1, "",\
+                                        style=wx.TE_MULTILINE|\
+                                              wx.TE_READONLY)
         # Creating the statusbar
         self.CreateStatusBar()
 
@@ -97,12 +109,23 @@ class TonicView(wx.Frame):
         # The third box split in 3 vertical box. YODAWG.
         v_sizer = wx.BoxSizer(wx.VERTICAL)
 
+        # notebook sizer box
+        note_v_sizer = wx.BoxSizer(wx.VERTICAL)
+        note_h_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        # Add text to notebook
+        note_v_sizer.Add(text_tab_desc, 0,\
+                         wx.ALIGN_CENTER_HORIZONTAL,\
+                         0)
+        self.tab_desc.SetSizer(note_v_sizer)
+
+
         # Addings tabs
-        self.description_tab.AddPage(self.description_tab_desc,
+        self.description_tab.AddPage(self.description_tab_desc,\
                                      _("description"))
-        self.description_tab.AddPage(self.description_tab_dep,
+        self.description_tab.AddPage(self.description_tab_dep,\
                                      _("dependencies"))
-        self.description_tab.AddPage(self.description_tab_chg,
+        self.description_tab.AddPage(self.description_tab_chg,\
                                      _("changes_list"))
 
         # Adding items
