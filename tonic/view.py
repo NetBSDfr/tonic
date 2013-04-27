89a063145ae72eba3f27b92bada986bbc9a9e664
Guillaume 'Llew' Delpierre <gde@llew.me>
2013-04-27 19:42:55 +0200

    Enable single selection on category list, not multiple

--- tonic/view.py
+++ tonic/view.py
@@ -36,34 +36,36 @@ class TonicView(wx.Frame):
         """Constructor."""
         wx.Frame.__init__(self, parent, title=title,\
                           pos=(-1, -1), size=(800,600),\
-                          style=wx.DEFAULT_FRAME_STYLE | wx.RESIZE_BORDER)
+                          style=wx.DEFAULT_FRAME_STYLE |\
+                                wx.RESIZE_BORDER)
         # Creating the categories list
         self.list_category = widgets.TonicCatListCtrl(self,
-                                                      style=wx.LC_REPORT|
-                                                          wx.LC_NO_HEADER|
-                                                          wx.LC_LIST|
-                                                          wx.SUNKEN_BORDER|
-                                                          wx.LC_SORT_ASCENDING)
+                                                      style=wx.LC_REPORT|\
+                                                           wx.LC_NO_HEADER|\
+                                                           wx.LC_LIST|\
+                                                           wx.SUNKEN_BORDER|\
+                                                           wx.LC_SORT_ASCENDING|\
+                                                           wx.LC_SINGLE_SEL)
 
         # Creating the searchbox
         self.search_box = wx.SearchCtrl(self, -1, "")
 
         # Creating the packages list
         self.list_pkg = widgets.TonicPkgListCtrl(self,
-                                                 style=wx.LC_REPORT|
-                                                      wx.SUNKEN_BORDER|
+                                                 style=wx.LC_REPORT|\
+                                                      wx.SUNKEN_BORDER|\
                                                       wx.LC_SORT_ASCENDING)
 
         # Creating suppl. informations about package panel
         self.description_tab = wx.Notebook(self, -1, style=wx.NB_BOTTOM)
-        self.description_tab_desc = wx.Panel(self.description_tab, -1,
-                                             style=wx.SUNKEN_BORDER|
+        self.description_tab_desc = wx.Panel(self.description_tab, -1,\
+                                             style=wx.SUNKEN_BORDER|\
                                                    wx.TAB_TRAVERSAL)
-        self.description_tab_dep = wx.Panel(self.description_tab, -1,
-                                            style=wx.SUNKEN_BORDER|
+        self.description_tab_dep = wx.Panel(self.description_tab, -1,\
+                                            style=wx.SUNKEN_BORDER|\
                                                   wx.TAB_TRAVERSAL)
-        self.description_tab_chg = wx.Panel(self.description_tab, -1,
-                                            style=wx.SUNKEN_BORDER|
+        self.description_tab_chg = wx.Panel(self.description_tab, -1,\
+                                            style=wx.SUNKEN_BORDER|\
                                                  wx.TAB_TRAVERSAL)
         self.text_tab_desc = wx.TextCtrl(self.description_tab_desc, -1, "",\
                                          style=wx.TE_MULTILINE|\
