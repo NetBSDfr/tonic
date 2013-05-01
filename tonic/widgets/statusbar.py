97fa78b311cd9567c4009f0f49ec831b11488722
Guillaume 'Llew' Delpierre <gde@llew.me>
2013-05-01 21:49:57 +0200

    add statusbar..

---
+++ tonic/widgets/statusbar.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2013 Guillaume Delpierre <gde@llew.me>
+# Copyright (c) 2013 Sylvain Mora <sylvain.mora@solevis.net>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer
+# in this position and unchanged.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""progressbar on statusbar."""
+
+import wx
+
+def statusbar():
+    self.statusbar = self.CreateStatusBar()
+    self.statusbar.SetFieldsCount(3)
+    self.statusbar.SetStatusWidths([320, -1, -2])
+
+    self.progress_bar = wx.Gauge(self.statusbar, -1,
+                                 style=wx.GA_HORIZONTAL|\
+                                       wx.GA_SMOOTH)
+    rect = self.statusbar.GetFieldRect(1)
+    
+    # set position on statusbar
+    self.progress_bar.SetPosition((rect.x+2, rect.y+2))
+    
+    # set size.
+    self.progress_bar.SetSize((rect.width-4, rect.height-4))
+
+    self.progress_bar.Hide()
+
+    # use this to show the statusbar
+    #self.progress_bar.Show() 
