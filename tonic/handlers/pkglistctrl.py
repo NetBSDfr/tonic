faebea1bcd370b4388b5c9503065b5b423c3fc86
Sylvain Mora <sylvain.mora@solevis.net>
2013-05-16 00:50:20 +0200

    Check if dep is already marked or not

--- tonic/handlers/pkglistctrl.py
+++ tonic/handlers/pkglistctrl.py
@@ -57,7 +57,8 @@ class TonicPkgListCtrlEvents(object):
                 self.view.list_pkg.SetItemBackgroundColour(index, "white")
                 self.model.unremove_pkg(pkg)
             elif not pkg in self.model.get_all_marked_pkgs():
-                deps = self.model.get_deps_name(pkg)
+                deps = list(set(self.model.get_deps_name(pkg)) - set(self.model.marked_pkgs.keys()))
+                print deps
                 if deps and not pkg in self.model.marked_pkgs.keys():
                     dlg = wx.SingleChoiceDialog(self.view, _("deps_dialog_title"), \
                                                     _("deps_add_dialog_caption"), \
